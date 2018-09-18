709ac0537a629c4a8c6b3260e48b54d4272a9d9f
Louis <ldelossa.ld@gmail.com>
2018-09-18 13:16:51 -0400

    login css working

--- src/Login.js
+++ src/Login.js
@@ -1,23 +1,39 @@
 import React, { Component } from 'react';
-import { Container, Input } from 'semantic-ui-react'
+import { Container, Input, Button } from 'semantic-ui-react'
 
 const divStyle = {
-	height: '100vh',
+	background: '#3E92CC',
 	display: 'flex',
+	justifyContents: 'center',
 	alignItems: 'center',
+	height: '100vh',
+	backgroundSize: 'cover',
+}
+
+const formStyle = {
+	margin: '3rem',
+	width: 'auto',
+}
+
+const submitButtonStyle = {
+	display: 'flex',
+	justifyContent: 'center',
 }
 
 class Login extends Component {
 	render() {
 		return (
 			<div style={divStyle}>
-				<Container >
+				<Container style={formStyle}>
 					<Input placeholder='username'/>
 					<br/>
 					<br/>
 					<Input placeholder='password'/>
 					<br/>
 					<br/>
+					<div style={submitButtonStyle}>
+						<Button>Submit</Button>
+					</div>
 				</Container>
 			</div>
 		)
